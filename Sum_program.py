aeb35c6fd23f7439e73e30c92abe0a74e552c43d
Bibek Phuyal <53286881+bibekphuyal@users.noreply.github.com>
2024-01-02 20:00:56 +1100

    Add files via upload

--- Sum_program.py
+++ Sum_program.py
@@ -10,3 +10,14 @@ for i in range(5):
 
 # This print  the result
 print(f"The sum of the  five numbers is: {total_sum}")
+
+#to display, if the sum of the number is even or odd
+if total_sum % 2 == 0:
+    #str(total_sum): Converts the integer total_sum to a string.
+#[0]: Accesses the first character of the resulting string.
+    first_digit = int(str(total_sum)[0])
+    print (f"Even number: {first_digit}")
+else:
+    #[1] Accesses the second character of the resulting string.
+    second_digit = int(str(total_sum)[1])
+    print (f"Odd number:{second_digit}")
